ee8425fb3c91d9affe89cce6ffef541803f9651a
Javarris Barnett <javarrisb@gmail.con>
2021-11-07 23:31:04 +0100

    added drag/drop feature to trash

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -101,6 +101,20 @@ $(".card .list-group").sortable( {
   }
 })
 
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
